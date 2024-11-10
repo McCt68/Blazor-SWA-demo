3789f0ebf2322609da29fd878158063d14fc474f
mcct <mcct@privat.dk>
2024-11-10 10:11:34 +0100

    Azurefunction API Response updated

---
+++ Api/AzureFuntionWeatherForecast.cs
@@ -0,0 +1,66 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace Api
+{
+    public class AzureFuntionWeatherForecast
+    {
+        private readonly ILogger<AzureFuntionWeatherForecast> _logger;
+
+        public AzureFuntionWeatherForecast(ILogger<AzureFuntionWeatherForecast> logger)
+        {
+            _logger = logger;
+        }        
+
+        // Inserted code from blog example.
+        [Function("WeatherForecast")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get")] HttpRequestData req)
+        {
+            var randomNumber = new Random();
+            var temp = 0;
+
+            // Create an array of 5 weatherforecast
+            var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = temp = randomNumber.Next(-20, 55),
+                Summary = GetSummary(temp)
+            }).ToArray();
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.WriteAsJsonAsync(result);
+            return response;
+        }
+
+        private string GetSummary(int temp)
+        {
+            var summary = "Mild";
+            if (temp >= 32)
+            {
+                summary = "Hot";
+            }
+            else if (temp <= 16 && temp > 0)
+            {
+                summary = "Cold";
+            }
+            else if (temp <= 0)
+            {
+                summary = "Freezing";
+            }
+            return summary;
+        }
+
+        public class WeatherForecast
+        {
+            public DateOnly Date { get; set; }
+            public int TemperatureC { get; set; }
+            public string Summary { get; set; }
+            public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        }
+
+    }
+}
